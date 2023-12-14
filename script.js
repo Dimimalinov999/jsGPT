086b658907e10acc3f568c8108aee7bbf1f7fbb6
DIMI <59560330+Dimimalinov999@users.noreply.github.com>
2023-12-14 21:06:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.clear()
+console.log("you need sum from here?")
+function button() {
+    let question = document.getElementById("input").value
+    if (question == "hey" || question == "hello") {
+        alert("Hello :)")
+     }
+    if (question == "nigga" || question == "fuck" || question == "sex" || question == "drugs") {
+        alert("Watch your language!")
+    }
+    if (question == "turn off your profanity filter") {
+        alert("donate at least $2 to unlock the ultimate version of jsGPT!")
+    }
+    
+    
+}
